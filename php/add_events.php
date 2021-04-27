1acb7b531454219a7944f1db96a8683316b5c60d
PhTung-99 <72152888+PhTung-99@users.noreply.github.com>
2021-04-27 19:19:14 +0700

    first commit

---
+++ php/add_events.php
@@ -0,0 +1,12 @@
+<?php 
+    include 'connect.php';
+    if (isset($_POST['submit']) && empty($_FILES['image']['tmp_name'])==false){
+        $file = addslashes(file_get_contents($_FILES['image']['tmp_name']));
+        $name = $_POST['news_name'];
+        $context = $_POST["textarea"];
+        $sql = "INSERT INTO `tintuc`(`id`, `TenTT`, `NoiDungTT`, `thumbnail`) VALUES ('','$name','$context','$file')";
+        if(mysqli_query($conn, $sql)){
+            echo 'dang ky tai cong';
+        }
+    }
+?>
